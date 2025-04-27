1bd5823edbcca5914707bf7afda54e3263d4bcfd
Razus228 <cassilasq2@gmail.com>
2025-04-27 16:34:50 +0300

    Saving

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 
 function App() {
+  const url ='https://api.api-ninjas.com/v1/cars';
+  const [carData, setCarData] = useState([{}]);
+
+  useEffect(() => {
+      fetch(`${url}`).then(
+          res => res.json()
+      ).then(
+        data => {
+          setCarData(data)
+          console.log(data)
+        }
+      )
+  }, []);
   return (
     <div>
+        {(typeof carData.results === 'undefiend') ? (
+          <div>
+            <p>Data is loading ...</p>
+          </div>
+        ): (
+          <div>
+            <p>Data has loaded successfully!</p>
+          </div>
+        )}
     </div>
   );
 }
 
+
+
+
 export default App;
+
+
